24911facbca08681019f339df65230508d209e04
maximecoulongeat <45207671+maximecoulongeat@users.noreply.github.com>
2018-11-23 11:16:51 +0100

    Update htmlcreator.py

--- htmlcreator.py
+++ htmlcreator.py
@@ -5,10 +5,12 @@ for x in range(0,20):
 
 f = create(variable1 + str(x).html)
 
+var1= (text + str(x))
+
 message = """<!DOCTYPE HTML>
 <html>
 <head></head>
-<body><p>Hello World, this is the variable (1 + str(x))!</p></body>
+<body><p>Hello World, this is the variable + var1 + .!</p></body>
 </html>"""
 
 f.write(message)
