57e8daccca5075914d0e1e331ecca68cb3478750
maximecoulongeat <45207671+maximecoulongeat@users.noreply.github.com>
2019-11-25 11:49:53 +0100

    Update webbrowser.message.py

--- webbrowser.message.py
+++ webbrowser.message.py
@@ -3,22 +3,24 @@ import webbrowser
 
 
 f = open('html_page.html','w')
-message1 = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+  message1 = """
+    <html><head><title>The Dormouse's story</title></head>
+    <body>
+    <p class="title"><b>The Dormouse's story</b></p>
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+    <p class="story">Once upon a time there were three little sisters; and their names were
+    <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+    and they lived at the bottom of a well.</p>
 
-<p class="story">...</p>
-"""
-
-
-f.write(message1)
-f.close(message)
-else
-return= 1
+    <p class="story">...</p>
+    """
+  
+  
+      and
+        f.write(message1)
+          f.close(message)
+        
+            else
+              return= 1
